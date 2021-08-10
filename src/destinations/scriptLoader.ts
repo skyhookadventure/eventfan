8d8d0308b2a39653c2b91dc33b4ef2501d629d5a
Alan Cooney <alan@skyhookadventure.com>
2021-08-10 17:59:55 +0100

    Add script loader helper

---
+++ src/destinations/scriptLoader.ts
@@ -0,0 +1,30 @@
+/**
+ * Helper to load a script in the browser
+ */
+export default async function scriptLoader(
+  id: string,
+  url: string
+): Promise<void> {
+  // Don't run if the script already exists
+  const exists = document.getElementById(id);
+  if (exists) return;
+
+  const isReady = new Promise<void>((resolve) => {
+    // Create the script
+    const scriptElement = document.createElement("script");
+    scriptElement.src = url;
+    scriptElement.async = true;
+    scriptElement.type = "text/javascript";
+    scriptElement.id = id;
+    document.body.appendChild(scriptElement);
+
+    function handleLoad() {
+      resolve();
+    }
+
+    // Set event listener
+    scriptElement.addEventListener("load", handleLoad);
+  });
+
+  await isReady;
+}
